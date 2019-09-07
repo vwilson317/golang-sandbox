1b764e2ebdaa92b095cee6ebf8bdefad92384720
vincent wilson <vincent.j.wilson317@gmail.com>
2019-09-06 22:48:11 -0400

    regen js file

--- api.js
+++ api.js
@@ -2,14 +2,9 @@
 exports.__esModule = true;
 var node_fetch_1 = require("node-fetch");
 function start() {
-    debugger;
     node_fetch_1["default"]('http://localhost:8080/api', {
         method: "POST",
-        body: "some data" //,
-        // headers: {
-        //     "Content-Type":"application/json"
-        // }
+        body: "some data"
     });
 }
 start();
-// export default api
